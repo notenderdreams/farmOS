5c3c245736f7cd83230fda001328703aa213a638
Nahian <enderdreams@outlook.com>
2026-02-10 13:19:00 +0600

    fix: tx print styling

--- src/cli/utils.h
+++ src/cli/utils.h
@@ -15,13 +15,13 @@ std::string getCurrentDate(){
 
 inline void printTransaction(const Transaction& tx, bool show_separator = true) {
     std::cout << asc::GREEN << "ID: " << asc::RESET << tx.tid << "\n";
-    std::cout << "  Type: " << tx::toStr(tx.type) << "\n";
-    std::cout << "  Direction: " << tx::toStr(tx.direction) << "\n";
-    std::cout << "  Amount: $" << tx.amount << "\n";
-    std::cout << "  Entity: " << tx::toStr(tx.entity_type) << " (" << tx.entity_id << ")\n";
-    std::cout << "  Description: " << tx.description << "\n";
-    std::cout << "  Date: " << tx.date << "\n";
-    std::cout << "  Status: " << tx::toStr(tx.status) << "\n";
+    std::cout << "\tType: " << tx::toStr(tx.type) << "\n";
+    std::cout << "\tDirection: " << tx::toStr(tx.direction) << "\n";
+    std::cout << "\tAmount: $" << tx.amount << "\n";
+    std::cout << "\tEntity: " << tx::toStr(tx.entity_type) << " (" << tx.entity_id << ")\n";
+    std::cout << "\tDescription: " << tx.description << "\n";
+    std::cout << "\tDate: " << tx.date << "\n";
+    std::cout << "\tStatus: " << tx::toStr(tx.status) << "\n";
     
     if (show_separator) {
         asc::printSeperator();
