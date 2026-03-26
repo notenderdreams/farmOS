ef63d39ecabc1b582fdfb9adf1e6d7f30f2bc998
Nahian <168405797+notenderdreams@users.noreply.github.com>
2026-03-26 07:51:46 -0700

    Revert "milk/egg to be taken from user per day"

--- src/app/handle_animal.h
+++ src/app/handle_animal.h
@@ -7,5 +7,3 @@ int animalList(const Args& args);
 int animalShow(const Args& args);
 int animalUpdateStatus(const Args& args);
 int animalDelete(const Args& args);
-int animalCollect(const Args& args);
-int animalProcess(const Args& args);
