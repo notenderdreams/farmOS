80835ec95a9456577e082b2858e715d94eb626ca
Talha <talhabinmonir10@gmail.com>
2026-03-27 23:55:14 +0600

    Delete employee folder and all its files

--- employee/hr.cpp
+++
@@ -1,8 +0,0 @@
-#include "hr.h"
-
-namespace hr {
-    const char* HRRoleStrs[]          = { "MANAGER", "ATTENDANCE_OFFICER", "ACCOUNTANT", "HIRING_OFFICER" };
-    const char* VacancyStatusStrs[]   = { "OPEN", "CLOSED", "FILLED" };
-    const char* ApplicantStatusStrs[] = { "PENDING", "SELECTED", "REJECTED" };
-    const char* AttendanceStatusStrs[]= { "PRESENT", "ABSENT", "GRANTED_LEAVE" };
-}
